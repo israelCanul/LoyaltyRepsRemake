c9e73d1aa106feaaacd94e4797f9058397759c3b
icanul-royalresorts <155753331+icanul-royalresorts@users.noreply.github.com>
2024-09-24 19:46:29 -0500

    working on videotraining items layout - adding shape from glide instead View

--- app/src/main/java/com/xcaret/loyaltyreps/adapter/SlideTrainingVideoAdapter.kt
+++ app/src/main/java/com/xcaret/loyaltyreps/adapter/SlideTrainingVideoAdapter.kt
@@ -6,6 +6,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.xcaret.loyaltyreps.R
 import com.xcaret.loyaltyreps.data.api.VideoTraining
 
 import com.xcaret.loyaltyreps.data.api.XPark
@@ -32,9 +36,19 @@ class SlideTrainingVideoAdapter(
     inner class TrainingVideoHolder(val itemBinding: ItemVideoTrainingBinding): RecyclerView.ViewHolder(itemBinding.root){
         fun bind(context: Context, video: VideoTraining){
             Log.i("ParkTraining", "$video")
+            itemBinding.nameQuizzVideo.text = video.name
+            itemBinding.pointsQuizzVideo.text = video.points.toString()
+            itemBinding.dealineQuizzVideo.text = video.deadline
 //            itemBinding.parkTitle.text = xpark.name
 //            itemBinding.parkTitle.setTextColor(Color.parseColor(xpark.color!!))
-            Glide.with(context).load(video.cover_img).into(itemBinding.imgTrainingVideoCover)
+            Glide.with(context)
+                .load(video.cover_img)
+                .fitCenter()
+                .apply(
+                    RequestOptions().transform(RoundedCorners(16))
+                    .error(R.drawable.bg_rounden_white).skipMemoryCache(true).diskCacheStrategy(
+                            DiskCacheStrategy.ALL))
+                .into(itemBinding.imgTrainingVideoCover)
         }
     }
 }
